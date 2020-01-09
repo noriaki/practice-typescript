93361a3f6f6fe36967dacbdf8a323d674b7106c8
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-09 19:13:03 +0900

    4-1-1: bad case (Runtime Error)

---
+++ chapter04-1/src/4-1-1/test.ts
@@ -0,0 +1,6 @@
+function getFormattedValue(value) {
+  return `${value.toFixed(1)} pt`
+}
+console.log(getFormattedValue(0.1))
+console.log(getFormattedValue(0))
+console.log(getFormattedValue(null))
