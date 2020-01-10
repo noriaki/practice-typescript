3da3b9f55b853b1f651946d63db7d43e1ce2ba42
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-10 18:37:55 +0900

    10-1-3: custom components

--- chapter10-1/src/pages/index.tsx
+++ chapter10-1/src/pages/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import { NextPage } from 'next'
+import Component from '../components'
 
-const IndexPage: NextPage = props => (
-  <div>Hello Next.js</div>
+type Props = {
+  title: string
+}
+type NextPageWithProps = NextPage<Props>
+
+const IndexPage: NextPageWithProps = ({ title }) => (
+  <>
+    <div>
+      <h1>{ title }</h1>
+    </div>
+    <Component />
+  </>
 )
+IndexPage.getInitialProps = async () => ({
+  title: 'Hello world with Next.js!'
+})
 
 export default IndexPage
