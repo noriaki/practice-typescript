fe840f71d3d4a2f29155ea33ad98b3a1ea5ed0ca
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2020-01-10 16:55:41 +0900

    7-1-6: refactor data that rename key, nullable, add row

--- chapter07-1/src/data.ts
+++ chapter07-1/src/data.ts
@@ -1,19 +1,24 @@
 export type Row = {
   id: string
-  generation: string
-  answers: number[]
+  age: string
+  answers: (number | null)[]
 }
 export type Rows = Row[]
 
 export const rows: Rows = [
   {
     id: 'up20-un30',
-    generation: '20〜30歳',
+    age: '20〜30歳',
     answers: [0.18, 0.22, 0.37, 0.23]
   },
   {
-    id: 'up30-up40',
-    generation: '30〜40歳',
+    id: 'up30-un40',
+    age: '30〜40歳',
     answers: [0.12, 0.28, 0.42, 0.18]
+  },
+  {
+    id: 'up50-un60',
+    age: '50〜60歳',
+    answers: [null, null, null, null]
   }
 ]
